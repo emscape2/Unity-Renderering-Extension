624a4c6da01bc84ad8dccaa898def168b43e77fb
emscape2 <emiel@silverentand.eu>
2020-09-29 19:46:19 +0200

    MAX COMMIT

---
+++ Scripts/Interactivity/ActionComponents/SwitchScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScene : Consequence
+{
+    public int Ind_scene;
+
+    public override void Engage()
+    {
+        SceneManager.LoadScene(Ind_scene);
+    }
+}
