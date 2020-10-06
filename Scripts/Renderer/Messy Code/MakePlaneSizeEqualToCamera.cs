471a86271a5817419ac6f1eb636dac1c2b454bf7
emscape2 <emiel@silverentand.eu>
2020-10-06 20:04:21 +0200

    Create MakePlaneSizeEqualToCamera.cs
    camsize

---
+++ Scripts/Renderer/Messy Code/MakePlaneSizeEqualToCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakePlaneSizeEqualToCamera : MonoBehaviour
+{
+
+    public float multipliah = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var aspect = Camera.main.aspect;
+        var height = Camera.main.orthographicSize * 2.0f * multipliah ;
+        transform.localScale = new Vector3(height * aspect, height, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
