b9ba625c772a0080b0c2ae81868405fc91cf77b6
emscape2 <emiel@silverentand.eu>
2020-10-06 22:12:55 +0200

    ttttt
    =

---
+++ Scripts/Renderer/Messy Code/MakeShiftXByX.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeShiftXByX : MonoBehaviour
+{
+    public float screenpercentage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.localPosition = new Vector3(screenpercentage, 0, 0.1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
