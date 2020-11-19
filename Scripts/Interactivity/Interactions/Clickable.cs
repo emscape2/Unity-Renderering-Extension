be3dad2fc4d8a2d60998b70d23726ab4eb144ddd
emscape2 <emiel@silverentand.eu>
2020-11-19 23:54:07 +0100

    Revert "Update Clickable.cs"
    This reverts commit 13bd3985a24b38d0b76aea36c9b6d60c0a3d82ad.

--- Scripts/Interactivity/Interactions/Clickable.cs
+++ Scripts/Interactivity/Interactions/Clickable.cs
@@ -1,58 +1,42 @@
-﻿using System;
-using System.Threading;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Clickable : Interaction
 {
     bool mouseDownLast, clicked;
-    public static DateTime tijd;
-
     public override bool? TryInteract(GameObject gameObject)
     {
-        if (tijd == null || DateTime.Now >= tijd)
-        {
-            //debug masks 
-            if (Input.GetKeyDown(KeyCode.U))
-            {
-                MouseBehavior.InstantiateDrawRect(gameObject);
-            }
 
-            var mouseDownNow = Input.GetMouseButton(0);
-            if (mouseDownLast && !mouseDownNow)
-            {
-                mouseDownLast = mouseDownNow;
-                if (clicked)
-                {
-                    clicked = false;
-                    if (MouseBehavior.MouseOver(Input.mousePosition, gameObject))
-                    {
-                        tijd = DateTime.Now.AddMilliseconds(500);
-                        return true;
-                    }
-                        
-                }
-                return false;
-            }
-            else if (!mouseDownLast && mouseDownNow)
-            {
-                mouseDownLast = mouseDownNow;
-                if (MouseBehavior.MouseOver(Input.mousePosition, gameObject))
-                {
-                    clicked = true;
-                }
-                return false;
-            }
+        //debug masks 
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            MouseBehavior.InstantiateDrawRect(gameObject);
+        }
+
+        var mouseDownNow = Input.GetMouseButton(0);
+        if (mouseDownLast && !mouseDownNow)
+        {
             mouseDownLast = mouseDownNow;
-            if (!mouseDownLast)
+            if (clicked)
+            {
                 clicked = false;
+                if (MouseBehavior.MouseOver(Input.mousePosition, gameObject))
+                    return true;
+            }
             return false;
         }
-        else
+        else if (!mouseDownLast && mouseDownNow)
         {
+            mouseDownLast = mouseDownNow;
+            if (MouseBehavior.MouseOver(Input.mousePosition, gameObject))
+            {
+                clicked = true;
+            }
             return false;
         }
-        
-
+        mouseDownLast = mouseDownNow;
+        if (!mouseDownLast)
+            clicked = false;
+        return false;
 
 
 
