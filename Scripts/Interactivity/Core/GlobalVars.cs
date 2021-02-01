d8dd5f6c68f86e44e824bdd9a08beba69bca6590
emscape2 <emiel@silverentand.eu>
2021-02-01 20:17:04 +0100

    Get Global Vars Set Global Vars Added

---
+++ Scripts/Interactivity/Core/GlobalVars.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace Assets.Scripts.Interactivity.ActionComponents
+{
+    public class GlobalVars
+    {
+        private Dictionary<string, int> globalVars;
+        private static GlobalVars thisObject;
+        public static GlobalVars getGlobalVars()
+        {
+            if (thisObject == null)
+            {
+                thisObject = new GlobalVars();
+                return thisObject;
+            }
+            else
+            {
+                return thisObject;
+            }
+        }
+
+        public void setVar(string var, int value)
+        {
+            if (globalVars.ContainsKey(var))
+                globalVars[var] = value;
+            else
+                globalVars.Add(var, value);
+        }
+
+        public int getVar(string var)
+        {
+            if (!globalVars.ContainsKey(var))
+            {
+                globalVars.Add(var, 0);
+            }
+            return globalVars[var];
+        }
+
+        private GlobalVars()
+        {
+            globalVars = new Dictionary<string,int>();
+        }
+    }
+}
---
+++ Scripts/Interactivity/Core/SetGlobalVarConsequence.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Interactivity.ActionComponents
+{
+
+    class SetGlobalVarConsequence : Consequence
+    {
+        public string VarToSet;
+        public int ValueToSet;
+        public void Awake()
+        {
+        }
+
+        public override void Disengage()
+        {
+            var global = GlobalVars.getGlobalVars();
+            global.setVar(VarToSet, ValueToSet);
+        }
+        public override void Engage()
+        {
+
+        }
+    }
+}
